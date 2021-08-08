fa8e2e82a4ead8094027f2de689ad14ab76706ab
zensolution <31325750+zensolution@users.noreply.github.com>
2021-08-08 16:11:32 -0400

    Create count_word.py

---
+++ count_word.py
@@ -0,0 +1,8 @@
+# Hint. You can use string split method to parse the words. Please refer to https://www.geeksforgeeks.org/python-string-split/
+
+def word_count(sentence):
+  pass
+
+# Sample output is like below
+#  {'the': 2, 'jumps': 1, 'brown': 1, 'lazy': 1, 'fox': 1, 'over': 1, 'quick': 1, 'dog.': 1} 
+print( word_count('the quick brown fox jumps over the lazy dog.'))
